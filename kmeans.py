bdf0f20b6b6fd2b98797a327e7366e42a49b9ef2
andre1393 <andre_1393@hotmail.com>
2018-11-10 15:54:34 -0200

    plot funcionando para mais de duas variaveis
    falta retornar métricas do treinamento

--- kmeans.py
+++ kmeans.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import math
 
 def kmeans(df, k = 2, max_iter = 5):
+    df['cluster'] = 1
     ridx = np.random.choice(range(1,len(df)), k)
     cluster = df.loc[ridx,].reset_index()
     for i in range(0, max_iter):
@@ -16,8 +17,8 @@ def kmeans(df, k = 2, max_iter = 5):
                     min_dist = c_dist
                     df.loc[i_row, 'cluster'] = i_cluster
         for i_cluster, c in cluster.iterrows():
-            cluster.loc[i_cluster,'x'] = df[df['cluster'] == i_cluster]['x'].mean()
-            cluster.loc[i_cluster,'y'] = df[df['cluster'] == i_cluster]['y'].mean()
+            for var in list(filter(lambda x: x != 'index', list(cluster))):
+                cluster.loc[i_cluster, var] = df[df['cluster'] == i_cluster][var].mean()
 
     return df, cluster
 
@@ -32,17 +33,27 @@ def plot_kmeans(df, cluster, color = None):
     if color == None:
         colors = ['blue', 'black', 'red', 'green', 'yellow', 'purple', 'gray']
         color = np.random.choice(colors, k, replace = False)
-        
-    for i in range(k):
-        plt.scatter(df[df['cluster'] == i]['x'], df[df['cluster'] == i]['y'], color = color[i], s = 10)
-        plt.scatter(cluster.loc[i, 'x'], cluster.loc[i, 'y'], color = color[i], marker='+', s = 1000)
+    
+    cols = list(filter(lambda x: x != 'index' and x != 'cluster', list(cluster)))
+    
+    for i in range(len(cols) - 1):
+        for j in range(i + 1,len(cols)):
+            fig, ax = plt.subplots()
+            for p in range(k):                
+                #plt.scatter(df[df['cluster'] == p]['x'], df[df['cluster'] == p]['y'], color = color[p], s = 10)
+                #plt.scatter(cluster.loc[p, 'x'], cluster.loc[p, 'y'], color = color[p], marker='+', s = 1000)
+                ax.scatter(df[df['cluster'] == p].iloc[:,i], df[df['cluster'] == p].iloc[:,j], color = color[p], s = 10)
+                ax.scatter(cluster.loc[p, cols[i]], cluster.loc[p, cols[j]], color = color[p], marker='+', s = 1000)
+                ax.set_title('Gráfico de {} versus {}'.format(cols[i], cols[j]))
+                ax.set_xlabel('{}'.format(cols[i]))
+                ax.set_ylabel('{}'.format(cols[j]))
+                ax.grid(True)
 
 def main():
-    x = np.random.normal(1, 0.1, 99) * np.repeat([4, 7, 10], 33)
-    y = np.random.normal(1, 0.1, 99) * np.repeat([2, 3, 4], 33)
-    z = np.random.normal(1, 0.1, 99) * np.repeat([2, 3, 4], 33)
+    x = np.random.normal(1, 0.1, 99) * np.repeat([5, 10, 15], 33)
+    y = np.random.normal(1, 0.1, 99) * np.repeat([5, 10, 15], 33)
+    z = np.random.normal(1, 0.1, 99) * np.repeat([7, 5, 4], 33)
     
-    df = pd.DataFrame({'x':x, 'y':y, 'z': z})
-    df['cluster'] = 1
-    df, cluster = kmeans(df, 2)
+    df = pd.DataFrame({'x':x, 'y':y})
+    df, cluster = kmeans(df, 3)
     plot_kmeans(df, cluster)
